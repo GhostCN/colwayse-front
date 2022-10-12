8a75b8d6e3773c6ab326d0d2a7aab1e232a81e44
Serigne Cheikh Mbacke SENE [GEBEYA DSI/DAC/DIF/DS] <ext_serignecheikhmbacke.sene@orange-sonatel.com>
2022-10-12 22:02:30 +0000

    change SSG to SSR

--- pages/index.js
+++ pages/index.js
@@ -26,24 +26,19 @@ export default function Index({products,sliders}) {
     </Layout>
   )
 }
-export async function getServerSideProps(ctx) {
-let sliders;
-let products;
-  let tokenResponse;
-  const jwt=parseCookies(ctx).jwt
- if(jwt!==undefined){
-     products=await Application.getData({token:jwt,url:ROUTE_ALL_PRODUCT})
-   sliders=await Application.getData({token:jwt,url:ROUTE_SLIDERS})
-  }else {
-     tokenResponse=await Application.auth()
-   setCookie(ctx, 'jwt', tokenResponse, {
-     maxAge: 30 * 24 * 60 * 60,
-     path: '/',
-   })
-    products=await Application.getData({token:tokenResponse,url:ROUTE_ALL_PRODUCT})
-    sliders=await Application.getData({token:tokenResponse,url:ROUTE_SLIDERS})
-  }
+
+export async function getStaticProps(){
+  let sliders;
+  let products;
+  var tokenResponse=await Application.auth()
+  setCookie('jwt', tokenResponse, {
+    maxAge: 30 * 24 * 60 * 60,
+    path: '/',
+  })
+  products=await Application.getData({token:tokenResponse,url:ROUTE_ALL_PRODUCT})
+  sliders=await Application.getData({token:tokenResponse,url:ROUTE_SLIDERS})
   return {
     props: {products,sliders}, // will be passed to the page component as props
+    revalidate: 1
   }
 }
